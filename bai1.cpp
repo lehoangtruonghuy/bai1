6584ea0f90efac7f5845fcb7b687472ba6388e43
lehoangtruonghuy <MAY 06@LAB2-17>
2024-11-18 09:38:26 +0700

    Add project files.

---
+++ bai1.cpp
@@ -0,0 +1,18 @@
+// bai1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "DIEN TICH VA CHU VI HINH CHU NHAT\n";
+	double  bankinh, chuvi, dientich, pi;
+	pi = 3.14;
+	cout << " nhap ban kinh: "; cin >> bankinh;
+
+	chuvi = 2 * pi * bankinh;
+	dientich = bankinh * bankinh * bankinh;
+
+	cout << " chu vi " << chuvi << endl;
+	cout << " dien tich " << dientich << endl;
+	return 0;
+}
